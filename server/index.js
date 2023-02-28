f64dea659014b89f6b25e2ddad20f50896a8f84d
sbartnicki <sbartnicki@icloud.com>
2023-02-28 16:18:17 -0500

    Updated mongo access string

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const app = express();
 const mongoose = require('mongoose');
 const users = require('./routes/users');
 const quizzes = require('./routes/quizzes');
+const mongoDbPass = process.env.DB_PASS;
 
 mongoose
-  .connect('mongodb://localhost/quiz-maker')
+  .connect(
+    `mongodb+srv://AdminUser:${mongoDbPass}@cluster1.lk3e6nd.mongodb.net/QuizMaker?retryWrites=true&w=majority`
+  )
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.log(err.message));
 
