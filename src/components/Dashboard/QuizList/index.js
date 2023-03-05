31f97cb38fa559e5dc89009b59492fbee1bb3944
sbartnicki <53477860+sbartnicki@users.noreply.github.com>
2023-03-05 13:32:28 -0500

    Server (#6)
    * Server branch first attempt
    
    * Updated Readme
    
    * Updated mongo access string
    
    * Added dotenv package
    
    * Merged latest from main
    
    * Added cors
    
    * API v1, can finish sprint 1
    
    * Frontend now talks with backend
    
    * Added email service
    
    * Enabled confirmation emails on Frontend
    
    ---------
    
    Co-authored-by: sbartnicki <sbartnicki@icloud.com>

--- src/components/Dashboard/QuizList/index.js
+++ src/components/Dashboard/QuizList/index.js
@@ -1,13 +1,13 @@
 import './styles.scss';
 
-export default function QuizzesList() {
+export default function QuizzesList({ quizTitles }) {
   return (
     <div className="quizzes-list-comp">
       <h3>My quizzes:</h3>
       <ul>
-        <li>Week 2 Quiz</li>
-        <li>MidTerm</li>
-        <li>Final Exam</li>
+        {quizTitles.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
       </ul>
     </div>
   );
