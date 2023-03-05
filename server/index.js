a4a2b73acf55b2ec40c81b4fd71fc64960683309
sbartnicki <sbartnicki@icloud.com>
2023-03-05 00:17:44 -0500

    Added cors

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const users = require('./routes/users');
 const quizzes = require('./routes/quizzes');
 const mongoDbPass = process.env.DB_PASS;
+const cors = require('cors');
 
 mongoose
   .connect(
@@ -13,6 +14,7 @@ mongoose
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.log(err.message));
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/quizzes', quizzes);
