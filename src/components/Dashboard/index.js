206302f1e49b286e6dfb532f4f4eef417ebf0f8d
sbartnicki <sbartnicki@icloud.com>
2023-03-05 15:53:36 -0500

    Fixed location.store issue

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,15 +1,18 @@
 import Header from './../Header';
 import QuizList from './QuizList';
-import CreateQuiz from './CreateQuiz';
 import Footer from '../Footer';
 import { useLocation } from 'react-router-dom';
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 import './styles.scss';
 
 export default function Dashboard() {
   const location = useLocation();
-  const quizTitles = location.state.map((item) => item.title);
+  let quizTitles = [];
+
+  if (location.state) {
+    quizTitles = location.state.map((item) => item.title);
+  }
 
   return (
     <div className="dashboard-comp">
