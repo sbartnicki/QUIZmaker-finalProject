beb16b1f151dfbcbb2159df8b788560130d48b79
sbartnicki <sbartnicki@icloud.com>
2023-03-25 20:51:15 -0400

    Added localstorage clearing in Header-handlesignout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,24 @@
-import "./styles.scss";
+import './styles.scss';
 import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
   const navigate = useNavigate();
 
   const handleSignOut = () => {
-    console.log("Signed Out");
-    navigate("/");
+    console.log('Signed Out');
+    localStorage.clear();
+    navigate('/');
   };
 
-    return (
-        <header className="main">
-            <div className="title">
-                <h1>QUIZ Maker</h1>
-                <p>Dashboard</p>
-            </div>
-            <button onClick={ handleSignOut } title="Sign Out" className="sign-out">
-                Sign out
-            </button>
-        </header>
-    );
+  return (
+    <header className="main">
+      <div className="title">
+        <h1>QUIZ Maker</h1>
+        <p>Dashboard</p>
+      </div>
+      <button onClick={handleSignOut} title="Sign Out" className="sign-out">
+        Sign out
+      </button>
+    </header>
+  );
 }
