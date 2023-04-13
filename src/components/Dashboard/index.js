6be8a63938ae2a1f245ff745bfc90d733bab2dbb
Julia Dias Fernandes <69637132+juliadiasfernandes@users.noreply.github.com>
2023-04-13 10:37:14 -0400

    Protected route to show dashboard

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,22 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Outlet } from 'react-router-dom';
 import Header from './../Header';
 import QuizList from './QuizList';
 import Footer from '../Footer';
-import { Outlet } from 'react-router-dom';
 
 import './styles.scss';
-import { useState } from 'react';
 
 export default function Dashboard() {
   const [trigger, setTrigger] = useState(1); //Updating of this reloads the quizlist
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const checkUserToken = () => {
+    const userToken = localStorage.getItem('token');
+    if (!userToken || userToken === 'undefined') {
+      setIsLoggedIn(false);
+    }
+    setIsLoggedIn(true);
+  }
+  useEffect(() => {
+    checkUserToken();
+  }, [isLoggedIn]);
+
   return (
     <div className="dashboard-comp">
-      <Header />
+      {isLoggedIn && <Header />}
       <div className="quiz-list-and-main">
-        <QuizList trigger={trigger} />
+        {isLoggedIn && <QuizList trigger={trigger} />}
         <Outlet context={[trigger, setTrigger]} />
       </div>
-      <Footer />
+      {isLoggedIn && <Footer />}
     </div>
   );
 }
